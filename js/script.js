0a35440a5b4c086a305ade43edb8e0c7a62f714a
cayce <cayce.coleman@outlook.com>
2022-05-11 06:01:58 -0700

    added repo list in order of newest to oldest and only 100 at a time

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //Global Variables//
 const overview = document.querySelector(".overview");
 const username = "caycecoleman";
+const repoListElement = document.querySelector(".repo-list");
 
 //Fetching info from Github API JSON data//
 const gitUserInfo= async function() {
@@ -27,5 +28,21 @@ const displayUserInfo= function(data) {
     <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
   </div>`;
   overview.append(div);
+  gitRepoList();
 };
 
+const gitRepoList= async function() {
+    const repoList = await fetch(
+        `https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const fetchRepos = await repoList.json();
+    displayRepos(fetchRepos);
+};
+
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    repoListElement.append(repoItem);
+    }
+};
